f06cdab843e7354d5be3ec1c30a7ba08c0c40717
ali ghanbari <ali110g@gmail.com>
2018-11-29 22:09:35 +0100

    First commit project created

---
+++ pokedex/pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Alí Gh on 29/11/2018.
+//  Copyright © 2018 Alí Gh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
